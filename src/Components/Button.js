b1977f49ab570d7643aef36e5c1012b0c2065d39
Kaleck Hamm <kaleckh@gmail.com>
2020-10-12 17:13:37 -0600

    storybook setup

---
+++ src/Components/Button.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Button() {
+    return(
+        <h1>jason</h1>
+    )
+}
+
+export default Button
---
+++ src/Components/Button.stories.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import  Button  from "./Button";
+
+export default {
+  title: "Example/Button",
+};
+
+export const defaultButton = () => {
+  return (
+    <div>
+      <Button></Button>
+    </div>
+  );
+};
