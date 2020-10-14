eb9ae1d52d51f4483a73a4e014d2c2766d94803a
Kaleck Hamm <kaleckh@gmail.com>
2020-10-13 19:37:00 -0600

    hooks working

--- src/Components/CreateTournament.js
+++ src/Components/CreateTournament.js
@@ -1,90 +1,70 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { createTournament } from "../ducks/reducer";
 import "./Createtournament.css";
 import axios from "axios";
 
-class CreateTournament extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tournamentType: null,
-      teamName: null,
-      date: null,
-    };
-  }
-  render() {
-    return (
-      <body>
-        {this.props.username}
-        <div className="newBox">
-          <div>
-            <label>Game type</label>{" "}
-            <input
-              onChange={(event) => {
-                this.setState({
-                  tournamentType: event.target.value,
-                });
-              }}
-            />
-            <label>Team names</label>{" "}
-            <input
-              onChange={(event) => {
-                this.setState({
-                  teamName: event.target.value.split(" "),
-                });
-              }}
-            />
-            <label>Date</label>
-            <input
-              onChange={(event) => {
-                this.setState({
-                  date: event.target.value,
-                });
-              }}
-            />
-            <button
-              onClick={() => {
-                
-                axios({
-                  method: "post",
-                  url: "http://localhost:3001/tournaments",
-                  data: {
-                    tournamentType: this.state.tournamentType,
-                    teamName: this.state.teamName,
-                    enrolled: 0,
-                    date: this.state.date,
-                  },
+function CreateTournament({ history }) {
+  let [teamName, setTeamName] = useState();
+  let [tournamentType, setTournamentType] = useState();
+  let [date, setDate] = useState();
+  let dispatch = useDispatch();
+  let username = useSelector((state) => state.username);
+  return (
+    <body>
+      {username}
+      <div className="newBox">
+        <div>
+          <label>Game type</label>{" "}
+          <input
+            onChange={(event) => {
+              setTournamentType(event.target.value);
+            }}
+          />
+          <label>Team names</label>{" "}
+          <input
+            onChange={(event) => {
+              setTeamName(event.target.value.split(" "));
+            }}
+          />
+          <label>Date</label>
+          <input
+            onChange={(event) => {
+              setDate(event.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              axios({
+                method: "post",
+                url: "http://localhost:3001/tournaments",
+                data: {
+                  tournamentType: tournamentType,
+                  teamName: teamName,
+                  enrolled: 0,
+                  date: date,
+                },
+              }).then((res) => {
+                let tournament = res.data;
 
-                }).then((res) => {
-                  let tournament = res.data;
-                  this.props.createTournament({
+                dispatch(
+                  createTournament({
                     tournamentType: tournament.tournamentType,
                     teamName: tournament.teamName,
                     enrolled: tournament.enrolled,
                     date: tournament.date,
-                  });
-                  this.props.history.push("/home");
-                });
-              }}
-            >
-              Save
-            </button>
-          </div>
+                  })
+                );
+                history.push("/home");
+              });
+            }}
+          >
+            Save
+          </button>
         </div>
-      </body>
-    );
-  }
+      </div>
+    </body>
+  );
 }
 
-function mapStateToProps(state) {
-  return {
-    username: state.username,
-  };
-}
-
-const mapDispatchToProps = {
-  createTournament,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(CreateTournament);
+export default CreateTournament;
--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -124,3 +124,4 @@ export {
   createUser,
   updateTournament,
 };
+
